b82a7490ca53233c3230a6f8578d47706a29f5f1
rserva <rserva@udel.edu>
2015-11-05 23:15:01 -0500

    Added SQL connector and queries

---
+++ API/SeatingChartAPI/SeatingChartAPI/Controllers/DBConnector.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.SqlClient;
+
+namespace SeatingChartAPI.Controllers
+{
+    public class DBConnector
+    {
+        static string cString = "server=us-cdbr-azure-central-a.cloudapp.net;"+
+                                "database=seatingChart;"+
+                                "Truested_Connection = yes"+
+                                "uid=bd4630ac6f11d6;"+
+                                "pwd=4089a061;";
+        static SqlConnection sqlcon;
+
+        public DBConnector()
+        {
+            sqlcon = new SqlConnection(cString);
+        }
+
+        public void connectToDB()
+        {
+            try
+            {
+                sqlcon.Open();
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public void disconnectFromDB()
+        {
+            try
+            {
+                sqlcon.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+
+        public Dictionary<string, string> getUserData(string userName)
+        {
+            Dictionary<string, string> userSet = new Dictionary<string, string>();
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("SELECT * FROM Users WHERE userName ="+userName, sqlcon);
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                userSet.Add("firstName", myReader["FirstName"].ToString() );
+                userSet.Add("lastName", myReader["LastName"].ToString());
+                userSet.Add("userName", myReader["Username"].ToString());
+                userSet.Add("city", myReader["City"].ToString());
+                userSet.Add("department", myReader["Department"].ToString());
+                userSet.Add("email", myReader["Email"].ToString());
+            }
+            return userSet;
+        }
+
+        public string getConnectedUser(string physicalAddr)
+        {
+            Dictionary<string, string> portSet = new Dictionary<string, string>();
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("SELECT * FROM InternalWebSites WHERE DevicePhysicalAddress =" + physicalAddr, sqlcon);
+            myReader = myCommand.ExecuteReader();
+            return myReader["Username"].ToString();
+        }
+
+        public Dictionary<string, Dictionary<string, string>> getPortInfo()
+        {
+            Dictionary<string, Dictionary<string, string>> switches = new Dictionary<string, Dictionary<string, string>>();
+            SqlDataReader myReader = null;
+            SqlCommand myCommand = new SqlCommand("SELECT * FROM NetworkSwitches", sqlcon);
+            myReader = myCommand.ExecuteReader();
+            while (myReader.Read())
+            {
+                Dictionary<string, string> switchSet = new Dictionary<string, string>();
+                switchSet.Add("port", myReader["SwitchPortName"].ToString());
+                switchSet.Add("physicalAddress", myReader["DevicePhysicalAddress"].ToString());
+                switches.Add(myReader["SwitchName"].ToString(), switchSet);
+
+            }
+            return switches;
+        }
+    }
+}
