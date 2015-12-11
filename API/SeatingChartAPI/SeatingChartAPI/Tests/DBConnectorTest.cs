decb02f4c37ab7c07b3cc08514647915d2b92c3e
ryan <rserva@udel.edu>
2015-12-11 17:10:55 -0500

    Fixed Tests

--- API/SeatingChartAPI/SeatingChartAPI/Tests/DBConnectorTest.cs
+++ API/SeatingChartAPI/SeatingChartAPI/Tests/DBConnectorTest.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Data.SqlClient;
+﻿using System.Collections.Generic;
 using NUnit.Framework;
+using System;
 
 namespace SeatingChartAPI.Controllers
 {
@@ -11,33 +8,49 @@ namespace SeatingChartAPI.Controllers
     public class DBConnectorTest
     {
         [Test]
-        public void DBConnectorTest1() // tests sucessful connection to database
+        public void DBConnectorTestConnection() // tests sucessful connection to database
         {
             DBConnector testConnection = new DBConnector();
             Assert.DoesNotThrow(new TestDelegate(testConnection.connectToDB));
             Assert.DoesNotThrow(new TestDelegate(testConnection.disconnectFromDB));
         }
         [Test]
-        public void DBConnecterTest2() // tests failed connection to database
+        public void DBConnecterTestErrorHanding() // tests failed connection to database
         {
             DBConnector testConnection2 = new DBConnector("notaserver.com","notadatabase","yes","notauid","notthepassword");
             Assert.Throws<System.Data.SqlClient.SqlException>(new TestDelegate(testConnection2.connectToDB));
         }
-        //[Test]
-        public void DBConnectorTest3() // tests data pulled from database
+        [Test]
+        public void DBConnectorTestGetUsername() // tests data pulled from database
+        {
+            DBConnector testConnection = new DBConnector();
+            try
+            {
+                testConnection.connectToDB();
+                Assert.AreEqual("jbuena", testConnection.getConnectedUser("349ADEAA08EA"));
+                testConnection.disconnectFromDB();
+            }
+            catch
+            {
+                Assert.Fail();
+            }
+        }
+        [Test]
+        public void DBConnectorTestGetData() // tests data pulled from database
         {
             DBConnector testConnection = new DBConnector();
             try
             {
                 testConnection.connectToDB();
                 Dictionary<string, string> testinfo = new Dictionary<string, string>();
-                testinfo = testConnection.getUserData("test"); // TODO: add this data to the DB so it doesn't fail :)
-                Assert.AreEqual(testinfo["FirstName"], "test");
-                Assert.AreEqual(testinfo["LastName"], "test");
-                Assert.AreEqual(testinfo["UserName"], "test12");
-                Assert.AreEqual(testinfo["City"], "testopolis");
-                Assert.AreEqual(testinfo["Department"], "department of test");
-                Assert.AreEqual(testinfo["Email"], "test@test.test");
+                testinfo = testConnection.getUserData("jbuena");
+                Assert.AreEqual(testinfo["firstName"], "Jamar");
+                Assert.AreEqual(testinfo["lastName"], "Buena");
+                Assert.AreEqual(testinfo["userName"], "jbuena");
+                Assert.AreEqual(testinfo["city"], "Conshohocken");
+                Assert.AreEqual(testinfo["department"], "Creative");
+                Assert.AreEqual(testinfo["email"], "jbuena@emoneyadvisor.com");
+                testConnection.disconnectFromDB();
             }
             catch
             {
